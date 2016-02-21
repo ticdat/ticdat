4229143344436e685ed5be5f24739e6c45c135bd
Pete Cacioppi <petercacioppi@Peters-iMac.local>
2016-02-20 20:32:42 -0800

    cleaned up

--- examples/ml_spam/ml_spam.py
+++ examples/ml_spam/ml_spam.py
@@ -15,9 +15,27 @@ from sklearn.learning_curve import learning_curve
 from ticdat import TicDatFactory
 
 
-dataFactory = TicDatFactory(messages = [[],["label", "message"]])
+dataFactory = TicDatFactory(messages = [[],["label", "message"]],
+                            parameters = [["key"], ["value"]])
+
+soln = TicDatFactory(predictions = [[],["label", "prediction"]],
+                     parameters = [["key"], ["value"]])
+
 
 def split_into_tokens(message):
     message = unicode(message, 'utf8')  # convert bytes into proper unicode
     return TextBlob(message).words
 
+
+def split_into_lemmas(message):
+    message = unicode(message, 'utf8').lower()
+    words = TextBlob(message).words
+    # for each word, take its "base form" = lemma
+    return [word.lemma for word in words]
+
+
+def run(td):
+    assert dataFactory.good_tic_dat_object(td)
+    messages = dataFactory.copy_to_pandas(td).messages
+    params = dict({"mode":"predict"}, **{k:r["value"] for k,r in td.parameters.items()})
+    assert params["mode"] in ["fit", "predict"]
