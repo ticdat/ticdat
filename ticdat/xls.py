98acb8433ec877e1dc7677fa759a0f1a98c7fd2d
Pete Cacioppi <petercacioppi@Peters-iMac.local>
2016-05-07 16:20:14 -0700

    added some spacing support

--- ticdat/xls.py
+++ ticdat/xls.py
@@ -46,7 +46,9 @@ class XlsTicFactory(freezable_factory(object, "_isFrozen")) :
         :return: a TicDat object populated by the matching sheets.
         caveats: Missing sheets resolve to an empty table, but missing fields
                  on matching sheets throw an Exception.
-                 Sheet names are considered case insensitive
+                 Sheet names are considered case insensitive, and white space is replaced
+                 with underscore for table name matching. (ticdat supports whitespace in
+                 field names but not table names).
                  Any field for which an empty string is invalid data and None is valid data
                  will replace the empty string with None. (This caveat requires the data_types
                  to be set for the ticDatFactory)
@@ -70,7 +72,7 @@ class XlsTicFactory(freezable_factory(object, "_isFrozen")) :
             raise TicDatError("Unable to open %s as xls file : %s"%(xls_file_path, e.message))
         sheets = defaultdict(list)
         for table, sheet in product(all_tables, book.sheets()) :
-            if table.lower() == sheet.name.lower() :
+            if table.lower() == sheet.name.lower().replace(' ', '_'):
                 sheets[table].append(sheet)
         duplicated_sheets = tuple(_t for _t,_s in sheets.items() if len(_s) > 1)
         verify(not duplicated_sheets, "The following sheet names were duplicated : " +
