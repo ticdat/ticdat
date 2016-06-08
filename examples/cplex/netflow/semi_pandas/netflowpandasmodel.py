6ce63dfa10251c92e1a18d780889a7c2b0c9a48a
Pete Cacioppi <petercacioppi@Peters-iMac.local>
2016-06-07 17:08:58 -0700

    worth remembering

--- examples/cplex/netflow/semi_pandas/netflowpandasmodel.py
+++ examples/cplex/netflow/semi_pandas/netflowpandasmodel.py
@@ -2,10 +2,12 @@
 
 # Copyright 2015, 2016 Opalytics, Inc.
 #
+# edited with permission from Gurobi Optimization, Inc.
 
 # Solve a multi-commodity flow problem as python package.
-# This version of the file doesn't use pandas at all, but instead uses ticdat
-# dict-of-dicts to represent data tables and gurobipy.tuplelist for slicing.
+# This version of the file uses pandas for data tables
+# but also iterates over table indicies explicitly and uses
+# Sloc to perform pandas slicing.
 
 # Implement core functionality needed to achieve modularity.
 # 1. Define the input data schema
@@ -13,9 +15,11 @@
 # 3. Create a solve function that accepts a data set consistent with the input
 #    schema and (if possible) returns a data set consistent with the output schema.
 
+
 # this version of the file uses CPLEX
 from docplex.mp.model import Model
-from ticdat import TicDatFactory, Slicer
+from ticdat import TicDatFactory, Sloc
+import pandas as pd
 
 # ------------------------ define the input schema --------------------------------
 dataFactory = TicDatFactory (
@@ -43,15 +47,17 @@ dataFactory.set_data_type("inflow", "quantity", min=-float("inf"),
                           inclusive_min=False)
 # ---------------------------------------------------------------------------------
 
+
 # ------------------------ define the output schema -------------------------------
 solutionFactory = TicDatFactory(
         flow = [["commodity", "source", "destination"], ["quantity"]])
 # ---------------------------------------------------------------------------------
 
 # ------------------------ solving section-----------------------------------------
-# the following are cplex credentials for cloud solving. Edit as needed.
 _cplex_url = "GET YOUR OWN FROM CPLEX"
 _cplex_key = "GET YOUR OWN FROM CPLEX"
+_cplex_key = "api_e22f42ce-fddd-4e98-a48a-e23ea8a79d0a"
+_cplex_url = "https://api-oaas.docloud.ibmcloud.com/job_manager/rest/v1/"
 def solve(dat):
     """
     core solving routine
@@ -62,16 +68,14 @@ def solve(dat):
 
     # Compute optimal solution
     if m.solve(url=_cplex_url, key=_cplex_key):
-        rtn = solutionFactory.TicDat()
         cplex_soln = m.solution
-        for (h, i, j),var in flow.items():
-            if cplex_soln.get_value(var) > 0:
-# ticdat recognizes flow as a one-data-field table, thus making write through easy
-                rtn.flow[h,i,j] = cplex_soln.get_value(var)
-        return rtn
+        t = flow.apply(lambda var : cplex_soln.get_value(var))
+        # TicDat is smart enough to handle a Series for a single data field table
+        return solutionFactory.freeze_me(solutionFactory.TicDat(flow = t[t > 0]))
 
 def create_model(dat):
     '''
+    utility function helpful for troubleshooting
     :param dat: a good ticdat for the dataFactory
     :return: a docplex model and dictionary of docplex flow variables
     '''
@@ -79,43 +83,39 @@ def create_model(dat):
     assert not dataFactory.find_foreign_key_failures(dat)
     assert not dataFactory.find_data_type_failures(dat)
 
+    dat = dataFactory.copy_to_pandas(dat, drop_pk_columns=False)
+
     # Create optimization model
     m = Model('netflow')
 
-    # Create variables
-    flow = {}
-    for h, i, j in dat.cost:
-        if (i,j) in dat.arcs:
-            flow[h,i,j] = m.continuous_var(name='flow_%s_%s_%s' % (h, i, j))
+    flow = Sloc.add_sloc(dat.cost.join(dat.arcs, on = ["source", "destination"],
+                                       how = "inner", rsuffix="_arcs").
+              apply(lambda r : m.addVar(ub=r.capacity, obj=r.cost,
+                            name= 'flow_%s_%s_%s' % (r.commodity, r.source, r.destination)),
+                    axis=1, reduce=True))
+    flow.name = "flow"
 
-    # docplex doesn't provide a native slicer so we use ticdat.Slicer
-    flowslice = Slicer(flow)
+    m.update()
 
-    # Arc capacity constraints
-    for i_,j_ in dat.arcs:
-        # docplex doesn't currently support float("inf") as RHS
-        if dat.arcs[i_,j_]["capacity"] < float("inf"):
-            m.add_constraint(m.sum(flow[h,i,j] for h,i,j in flowslice.slice('*',i_, j_))
-                         <= dat.arcs[i_,j_]["capacity"],
-                         ctname = 'cap_%s_%s' % (i_, j_))
-
+    dat.arcs.join(flow.groupby(level=["source", "destination"]).sum()).apply(
+        lambda r : m.addConstr(r.flow <= r.capacity,
+                               'cap_%s_%s'%(r.source, r.destination)), axis =1)
 
     # for readability purposes using a dummy variable thats always zero
-    zero = m.continuous_var(lb=0, ub=0, name = "forcedToZero")
+    zero = m.addVar(lb=0, ub=0, name = "forcedToZero")
 
-    # Flow conservation constraints. Constraints are generated only for relevant pairs.
-    # So we generate a conservation of flow constraint if there is negative or positive inflow
-    # quantity, or at least one inbound flow variable, or at least one outbound flow variable.
-    for h_,j_ in set(k for k,v in dat.inflow.items() if abs(v["quantity"]) > 0).union(
-            {(h,i) for h,i,j in flow}, {(h,j) for h,i,j in flow}) :
-        m.add_constraint(
-          (m.sum(flow[h,i,j] for h,i,j in flowslice.slice(h_,'*',j_)) or zero) +
-              dat.inflow.get((h_,j_), {"quantity":0})["quantity"] ==
-          (m.sum(flow[h,i,j] for h,i,j in flowslice.slice(h_, j_, '*')) or zero),
-                   ctname = 'node_%s_%s' % (h_, j_))
+    m.update()
 
-    # setting the objective function
-    m.minimize(m.sum(flow * dat.cost[h, i, j]["cost"] for (h, i, j),flow in flow.items()))
+    # there is a more pandonic way to do this group of constraints, but lets
+    # demonstrate .sloc for those who think it might be more intuitive
+    for h_,j_ in sorted(set(dat.inflow[abs(dat.inflow.quantity) > 0].index).union(
+        flow.groupby(level=['commodity','source']).groups.keys(),
+        flow.groupby(level=['commodity','destination']).groups.keys())):
+            m.addConstr((quicksum(flow.sloc[h_,:,j_]) or zero) +
+                        dat.inflow.quantity.loc[h_,j_] ==
+                        (quicksum(flow.sloc[h_,j_,:]) or zero),
+                        'node_%s_%s' % (h_, j_))
 
+    m.update()
     return m, flow
 # ---------------------------------------------------------------------------------
