31d2fbe963abad432f7618f0f4371a40b680fcd9
Pete Cacioppi <Peter.Cacioppi@gmail.com>
2016-08-06 10:37:26 -0700

    minor comment goof

--- examples/cplex/cog/cogmodel.py
+++ examples/cplex/cog/cogmodel.py
@@ -155,8 +155,8 @@ def solve(dat, out, err, progress):
         progress.numerical_progress("Core Optimization", 100)
         cplex_soln = m.solution
         sln = solutionFactory.TicDat()
-        sln.parameters["Lower Bound"] = cplex_soln.get_objective_value() # appears correct
-        sln.parameters["Upper Bound"] = cplex_soln.get_objective_value() # NEEDS FIX http://ibm.co/2aQwKYG
+        sln.parameters["Lower Bound"] = cplex_soln.get_objective_value() # NEEDS FIX http://ibm.co/2aQwKYG
+        sln.parameters["Upper Bound"] = cplex_soln.get_objective_value() # appears correct
         out.write('Upper Bound: %g\n' % sln.parameters["Upper Bound"]["value"])
         out.write('Lower Bound: %g\n' % sln.parameters["Lower Bound"]["value"])
 
