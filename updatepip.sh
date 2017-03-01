b6a742e2d69aa18cbf084b9c0706aa56fd7cb1b8
Sam Nelson <snelson@opalytics.com>
2017-03-01 14:16:13 -0800

    remove extra file from source control

--- updatepip.sh
+++
@@ -1,5 +0,0 @@
-python setup.py sdist bdist_wheel
-yes | pip uninstall ticdat
-yes | pip install dist/ticdat-0.2.5.tar.gz
-cp dist/ticdat-0.2.5.tar.gz ../opalytics-lenticular/lenticular
-cp -rf ticdat/* ../opalytics-lenticular/lenticular/oplstuff/ticdat
