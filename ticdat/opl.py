c3311b429daf36c7c35c793b308030d7205704d2
Sam Nelson <snelson@opalytics.com>
2017-03-10 11:38:02 -0800

    Revert "Opl keywords"

--- ticdat/opl.py
+++ ticdat/opl.py
@@ -1,21 +1,9 @@
 from ticdat.utils import verify, containerish, stringish, find_duplicates_from_dict_ticdat
 import ticdat.utils as tu
-from ticdat.ticdatfactory import TicDatFactory
 import os, subprocess, inspect, time, uuid, shutil
 from collections import defaultdict
-from ticdat.jsontd import make_json_dict
 
 INFINITY = 999999
-
-opl_keywords = ["initial", "template", "struct", "all", "and", "assert", "boolean", "constraints", "CP",
-                "CPLEX", "cumulFunction", "DBConnection", "DBExecute", "DBRead", "DBUpdate", "dexpr",
-                "diff", "div", "dvar", "else", "execute", "false", "float", "float+", "forall", "from",
-                "in", "if", "include", "infinity", "int", "int+", "intensity", "inter", "interval",
-                "invoke", "key", "main", "max", "maximize", "maxint", "min", "minimize", "mod", "not",
-                "optional", "or", "ordered", "piecewise", "prepare", "prod", "pwlFunction", "range",
-                "reversed", "sequence", "setof", "SheetConnection", "SheetRead", "SheetWrite", "size",
-                "sorted", "SPSSConnection", "SPSSRead", "stateFunction", "stepFunction", "stepwise",
-                "string", "subject", "sum", "symdiff", "to", "true", "tuple", "types", "union", "using", "with"]
 
 def _code_dir():
     return os.path.dirname(os.path.abspath(inspect.getsourcefile(_code_dir)))
@@ -51,54 +39,18 @@ def pattern_finder(string, pattern, rsearch=False):
     return False
 
 def _find_case_space_duplicates(tdf):
-    """
-    Finds fields that are case space duplicates
-    :param tdf: A TicDatFactory defining the schema
-    :return: A dictionary with the keys being tables that have case space duplicates
-    """
+    '''Need to loop through the tables'''
+    '''compare case insensitive versions of the fields in a table'''
+    '''if there's a dup, that table is one of the ones returned'''
+    '''hi'''
     schema = tdf.schema()
     tables_with_case_insensitive_dups = {}
-    for table in schema:
+    for table in schema.keys():
         fields = set(schema[table][0]).union(schema[table][1])
         case_insensitive_fields = set(map(lambda k: k.lower().replace(" ", "_"),fields))
         if len(fields) != len(case_insensitive_fields):
             tables_with_case_insensitive_dups[table] = fields
     return tables_with_case_insensitive_dups
-
-def _change_fields_with_opl_keywords(tdf, undo=False):
-    tdf_schema = tdf.schema()
-    mapping = {}
-    for table, fields in tdf_schema.items():
-        for fields_list in [fields[0], fields[1]]:
-            for findex in range(len(fields_list)):
-                original_field = fields_list[findex]
-                if not undo:
-                    verify(not fields_list[findex].startswith('_'),
-                           ("Field names cannot start with '_', in table %s : " +
-                            "field is %s") % (table, fields_list[findex]))
-                    if fields_list[findex].lower() in opl_keywords:
-                        fields_list[findex] = '_' + fields_list[findex]
-                else:
-                    if fields_list[findex].startswith('_'):
-                        fields_list[findex] = fields_list[findex][1:]
-                mapping[table,original_field] = fields_list[findex]
-
-    rtn = TicDatFactory(**tdf_schema)
-    for (table, original_field),new_field in mapping.items():
-        if original_field in tdf.default_values.get(table, ()):
-            rtn.set_default_value(table, new_field,
-                                  tdf.default_values[table][original_field])
-        if original_field in tdf.data_types.get(table, ()):
-            rtn.set_data_type(table, new_field,
-                              *(tdf.data_types[table][original_field]))
-    rtn.opl_prepend = tdf.opl_prepend
-    return rtn
-
-def _fix_fields_with_opl_keywords(tdf):
-    return _change_fields_with_opl_keywords(tdf)
-
-def _unfix_fields_with_opl_keywords(tdf):
-    return _change_fields_with_opl_keywords(tdf, True)
 
 def opl_run(mod_file, input_tdf, input_dat, soln_tdf, infinity=INFINITY, oplrun_path=None, post_solve=None):
     """
@@ -118,16 +70,10 @@ def opl_run(mod_file, input_tdf, input_dat, soln_tdf, infinity=INFINITY, oplrun_
            len(input_tdf.all_tables) + len(soln_tdf.all_tables),
            "There are colliding input and solution table names.\nSet opl_prepend so " +
            "as to insure the input and solution table names are effectively distinct.")
+    mod_file_name = os.path.basename(mod_file)[:-4]
     msg  = []
     verify(input_tdf.good_tic_dat_object(input_dat, msg.append),
            "tic_dat not a good object for the input_tdf factory : %s"%"\n".join(msg))
-    orig_input_tdf, orig_soln_tdf = input_tdf, soln_tdf
-    input_tdf = _fix_fields_with_opl_keywords(input_tdf)
-    soln_tdf = _fix_fields_with_opl_keywords(soln_tdf)
-    input_dat = input_tdf.TicDat(**make_json_dict(orig_input_tdf, input_dat))
-    assert input_tdf.good_tic_dat_object(input_dat)
-    mod_file_name = os.path.basename(mod_file)[:-4]
-
     working_dir = os.path.abspath(os.path.dirname(mod_file))
     if tu.development_deployed_environment:
         working_dir = os.path.join(working_dir, "oplticdat_%s"%uuid.uuid4())
@@ -147,10 +93,10 @@ def opl_run(mod_file, input_tdf, input_dat, soln_tdf, infinity=INFINITY, oplrun_
     verify(os.path.isfile(datfile), "Could not create temp.dat")
     with open(os.path.join(working_dir, "ticdat_"+mod_file_name+".mod"), "w") as f:
         f.write("/* Autogenerated input file, created by opl.py on " + time.asctime() + " */\n")
-        f.write(create_opl_mod_text(orig_input_tdf))
+        f.write(create_opl_mod_text(input_tdf))
     with open(os.path.join(working_dir,"ticdat_"+mod_file_name+"_output.mod"), "w") as f:
         f.write("/* Autogenerated output file, created by opl.py on " + time.asctime() + " */\n")
-        f.write(create_opl_mod_output_text(orig_soln_tdf))
+        f.write(create_opl_mod_output_text(soln_tdf))
     if not oplrun_path:
         verify(os.path.isfile(os.path.join(_code_dir(),"oplrun_path.txt")),
                "need to either pass oplrun_path argument or run oplrun_setup.py")
@@ -166,8 +112,6 @@ def opl_run(mod_file, input_tdf, input_dat, soln_tdf, infinity=INFINITY, oplrun_
     try:
         output = subprocess.check_output([oplrun_path, mod_file, datfile], stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as err:
-        if tu.development_deployed_environment:
-            raise Exception("oplrun failed to complete: " + err.output)
         output = err.output
     with open(output_txt, "w") as f:
         f.write(output)
@@ -178,7 +122,6 @@ def opl_run(mod_file, input_tdf, input_dat, soln_tdf, infinity=INFINITY, oplrun_
         output = f.read()
     if post_solve:
         post_solve()
-    soln_tdf = _unfix_fields_with_opl_keywords(soln_tdf)
     return read_opl_text(soln_tdf, output, False)
 
 _can_run_oplrun_tests = os.path.isfile(os.path.join(_code_dir(),"oplrun_path.txt"))
@@ -246,11 +189,9 @@ def create_opl_mod_output_text(tdf):
     return _create_opl_mod_text(tdf, True)
 
 def _create_opl_mod_text(tdf, output):
-    verify(not _find_case_space_duplicates(tdf), "There are case space duplicate field names in the schema.")
     verify(not tdf.generator_tables, "Input schema error - doesn't work with generator tables.")
     verify(not tdf.generic_tables, "Input schema error - doesn't work with generic tables. (not yet - will \
             add ASAP as needed) ")
-    tdf = _fix_fields_with_opl_keywords(tdf)
     rtn = ''
     dict_tables = {t for t, pk in tdf.primary_key_fields.items() if pk}
     verify(set(dict_tables) == set(tdf.all_tables), "not yet handling non-PK tables of any sort")
@@ -271,11 +212,11 @@ def _create_opl_mod_text(tdf, output):
         else:
             rtn += "tuple " + prepend + tbn + "_type\n{"
             for pk in tdf.primary_key_fields[tbn]:
-                pk_m = pk.replace(' ', '_').lower()
-                rtn += "\n\tkey " + get_type(tdf.data_types, tbn, pk) + " " + pk_m + ";"
+                pk.lower().replace(' ', '_')
+                rtn += "\n\tkey " + get_type(tdf.data_types, tbn, pk) + " " + pk + ";"
             for df in tdf.data_fields[tbn]:
-                df_m = df.replace(' ', '_').lower()
-                rtn += "\n\t" + get_type(tdf.data_types, tbn, df) + " " + df_m + ";"
+                df.lower().replace(' ', '_')
+                rtn += "\n\t" + get_type(tdf.data_types, tbn, df) + " " + df + ";"
             rtn += "\n};\n\n{" + prepend + tbn + "_type} " + prepend + tbn + "=" + sig + ";\n\n"
         return rtn
 
