b095e48994f53ceccd25be1cb414fb8981e4913e
Sam Nelson <snelson@opalytics.com>
2017-05-12 16:19:31 -0700

    support lingo_prepend

--- ticdat/ticdatfactory.py
+++ ticdat/ticdatfactory.py
@@ -71,7 +71,7 @@ class _TypeDictionary(namedtuple("TypeDictionary",
             return bool(self.nullable)
         return False
 
-class TicDatFactory(freezable_factory(object, "_isFrozen", {"opl_prepend"})) :
+class TicDatFactory(freezable_factory(object, "_isFrozen", {"opl_prepend", "lingo_prepend"})) :
     """
     Primary class for ticdat library. This class is constructed with a schema,
     and can be used to generate TicDat objects, or to write TicDat objects to
@@ -1337,10 +1337,19 @@ foreign keys, the code throwing this exception will be removed.
     def opl_prepend(self):
         return self._prepends.get("opl", "")
 
+    @property
+    def lingo_prepend(self):
+        return self._prepends.get("lingo", "")
+
     @opl_prepend.setter
     def opl_prepend(self, value):
         verify(utils.stringish(value), "opl_prepend should be a string")
         self._prepends["opl"] = value
+
+    @lingo_prepend.setter
+    def lingo_prepend(self,value):
+        verify(utils.stringish(value), "lingo_prepend should be a string")
+        self._prepends["lingo"] = value
 
 def freeze_me(x) :
     """
