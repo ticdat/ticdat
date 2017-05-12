e85ff6df93d679068695244d2aae33299e571288
Sam Nelson <snelson@opalytics.com>
2017-05-12 16:02:08 -0700

    first pass at lingo.py

--- ticdat/__init__.py
+++ ticdat/__init__.py
@@ -18,7 +18,8 @@ from ticdat.ticdatfactory import TicDatFactory, freeze_me
 from ticdat.utils import Sloc, LogFile, Progress, Slicer, \
                          find_denormalized_sub_table_failures, standard_main
 from ticdat.opl import opl_run, create_opl_mod_text, create_opl_mod_output_text
+from ticdat.lingo import lingo_run
 from ticdat.model import Model
 __all__ = ["TicDatFactory", "freeze_me", "LogFile", "Sloc", "Slicer", "Progress", "standard_main", "Model", "opl_run",
-           "create_opl_mod_text", "create_opl_mod_output_text"]
+           "create_opl_mod_text", "create_opl_mod_output_text", "lingo_run"]
 
