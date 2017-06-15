7087f7f20a9595b8670dfe24ba2cc7b2dbed6400
pjcpjc <peter.cacioppi@gmail.com>
2017-06-15 11:07:00 -0700

    pretty up diet with re: to infinity

--- examples/diet/create_xlsx_file.py
+++
@@ -1,3 +0,0 @@
-from diet import input_schema
-dat = input_schema.sql.create_tic_dat_from_sql("diet_sample_data.sql")
-input_schema.xls.write_file(dat, "Diet_Sample_Data.xlsx", allow_overwrite=True)
