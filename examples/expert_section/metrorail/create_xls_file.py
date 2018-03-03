d625a5a56ace72c92d00069dac9e0d5a6f4b4dde
pjcpjc <peter.cacioppi@gmail.com>
2018-03-03 14:15:36 -0800

    create file fixed

--- examples/expert_section/metrorail/create_xls_file.py
+++ examples/expert_section/metrorail/create_xls_file.py
@@ -1,3 +1,3 @@
 from metrorail import input_schema
-dat = input_schema.json.create_tic_dat_from_sql("metrorail_sample_data.json")
-input_schema.xlsx.write_file(dat, "Metro Rail Data.xlsx", allow_overwrite=True)
+dat = input_schema.json.create_tic_dat("metrorail_sample_data.json")
+input_schema.xls.write_file(dat, "Metro Rail Data.xlsx", allow_overwrite=True)
