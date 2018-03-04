a1900b8d1370cb026f86e799e3ac9a2065141b63
Pete Cacioppi <petercacioppi@ip-192-168-1-106.us-west-2.compute.internal>
2018-03-04 13:24:23 -0800

    cleaner excel file example

--- examples/expert_section/metrorail/create_xls_file.py
+++ examples/expert_section/metrorail/create_xls_file.py
@@ -1,3 +1,4 @@
 from metrorail import input_schema
 dat = input_schema.json.create_tic_dat("metrorail_sample_data.json")
-input_schema.xls.write_file(dat, "Metro Rail Data.xlsx", allow_overwrite=True)
+input_schema.xls.write_file(dat, "Metro Rail Data.xlsx", allow_overwrite=True,
+                            case_space_sheet_names=True)
