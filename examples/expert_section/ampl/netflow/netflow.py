c77a73e4cea438755f89f39b6c28d4e06016d9e7
pjcpjc <peter.cacioppi@gmail.com>
2018-05-07 20:54:20 -0700

    better sparse handling from amplpy netflow

--- examples/expert_section/ampl/netflow/netflow.py
+++ examples/expert_section/ampl/netflow/netflow.py
@@ -15,6 +15,7 @@
 
 from ticdat import TicDatFactory, standard_main
 from amplpy import AMPL
+from itertools import product
 
 # ------------------------ define the input schema --------------------------------
 input_schema = TicDatFactory (
@@ -63,6 +64,10 @@ def solve(dat):
     assert not input_schema.find_foreign_key_failures(dat)
     assert not input_schema.find_data_type_failures(dat)
 
+    # our AMPL code assumes inflow is fully populated so lets make it so
+    for c,n in product(dat.commodities, dat.nodes):
+        dat.inflow[c,n] = dat.inflow[c,n] # ticdat will act like a default dict here and insert zeros as needed
+
     # copy the data over to amplpy.DataFrame objects, renaming the data fields as needed
     dat = input_schema.copy_to_ampl(dat, field_renamings={("arcs", "Capacity"): "capacity",
             ("cost", "Cost"): "cost", ("inflow", "Quantity"): "inflow"})
@@ -73,25 +78,21 @@ def solve(dat):
     set NODES;
     set ARCS within {i in NODES, j in NODES: i <> j};
     set COMMODITIES;
-
     param capacity {ARCS} >= 0;
-    param cost {COMMODITIES,ARCS} > 0;
+    set SHIPMENT_OPTIONS within {COMMODITIES,ARCS};
+    param cost {SHIPMENT_OPTIONS} > 0;
     param inflow {COMMODITIES,NODES};
-
-    var Flow {COMMODITIES,ARCS} >= 0;
-
+    var Flow {SHIPMENT_OPTIONS} >= 0;
     minimize TotalCost:
-       sum {h in COMMODITIES, (i,j) in ARCS} cost[h,i,j] * Flow[h,i,j];
-
+       sum {(h,i,j) in SHIPMENT_OPTIONS} cost[h,i,j] * Flow[h,i,j];
     subject to Capacity {(i,j) in ARCS}:
-       sum {h in COMMODITIES} Flow[h,i,j] <= capacity[i,j];
-
+       sum {(h,i,j) in SHIPMENT_OPTIONS} Flow[h,i,j] <= capacity[i,j];
     subject to Conservation {h in COMMODITIES, j in NODES}:
-       sum {(i,j) in ARCS} Flow[h,i,j] + inflow[h,j] = sum {(j,i) in ARCS} Flow[h,j,i];
+       sum {(h,i,j) in SHIPMENT_OPTIONS} Flow[h,i,j] + inflow[h,j] = sum {(h,j,i) in SHIPMENT_OPTIONS} Flow[h,j,i];
     """)
 
     input_schema.set_ampl_data(dat, ampl, {"nodes": "NODES", "arcs": "ARCS",
-                                           "commodities": "COMMODITIES"})
+                                       "commodities": "COMMODITIES", "cost":"SHIPMENT_OPTIONS"})
     ampl.solve()
 
     if ampl.getValue("solve_result") != "infeasible":
