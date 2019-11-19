0c8249faacad209d1286f3e3cad6fc21c77cd7f7
Pete Cacioppi <Peter.Cacioppi@gmail.com>
2019-11-19 14:24:12 -0800

    0.2.18.7 release
    adds PostGres and datetime data type support

--- ticdat/__init__.py
+++ ticdat/__init__.py
@@ -1,15 +1,10 @@
 """
-ticDat module for reading and writing data modules in ticDat format.
-ticDat is a simple, easy-to-read format for tabular data. It is partly inspired by csv.DictReader
-and csv.DictWriter.  When primary keys are specified, each table is a dictionary of dictionaries.
-Otherwise, each table is an enumerable of dictionaries (as in DictReader/DictWriter). When foreign keys are
-specified, they are used to create links between the "row dictionaries" of the parent table to the matching
-"row dictionaries" of the child objects.
-Although ticDat was specifically designed with Mixed Integer Programming data sets in mind, it can be used for
-rapidly developing a wide variety of mathematical engines. It facilitates creating one definition of your
-input data schema and one solve module, and reusing this same code, unchanged, on data from different
-sources. This "separation of model from data" enables a user to move easily from toy, hard coded data to
-larger, more realistic data sets.
+ticdat is a Python package that provides lightweight, ORM style functionality around either a dict-of-dicts or
+pandas.DataFrame representation of tables. Although ticDat was specifically designed with Mixed Integer Programming
+data sets in mind, it can be used for rapidly developing a wide variety of mathematical engines. It facilitates
+creating one definition of your input data schema and one solve module, and reusing this same code, unchanged,
+on data from different sources. This "separation of model from data" enables a user to move easily from toy,
+hard coded data to larger, more realistic data sets.
 The ticDat library is distributed under the BSD2 open source license.
 """
 
@@ -19,7 +14,7 @@ from ticdat.utils import Sloc, LogFile, Progress, Slicer, standard_main, \
 from ticdat.opl import opl_run, create_opl_mod_text, create_opl_mod_output_text
 from ticdat.model import Model
 from ticdat.pandatfactory import PanDatFactory
-__version__ = '0.2.18.6'
+__version__ = '0.2.18.7'
 __all__ = ["TicDatFactory", "PanDatFactory", "freeze_me", "LogFile", "Sloc", "Slicer", "Progress", "standard_main",
            "Model", "opl_run", "create_opl_mod_text", "create_opl_mod_output_text", "ampl_format",
            "gurobi_env", "verify"]
