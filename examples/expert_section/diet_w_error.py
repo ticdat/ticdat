4dd2042d129de879cab99b0818f406952b2bcfd6
Pete Cacioppi <Peter.Cacioppi@gmail.com>
2021-10-11 18:48:23 -0700

    advanced example - test error in solve code

---
+++ examples/expert_section/diet_w_error.py
@@ -0,0 +1,72 @@
+# Same as diet.py except it has an error.
+
+try: # if you don't have gurobipy installed, the code will still load and then fail on solve
+    import gurobipy as gp
+except:
+    gp = None
+
+from ticdat import TicDatFactory, standard_main
+
+# ------------------------ define the input schema --------------------------------
+# There are three input tables, with 4 primary key fields and 4 data fields.
+input_schema = TicDatFactory (
+    categories=[["Name"], ["Min Nutrition", "Max Nutrition"]],
+    foods=[["Name"], ["Cost"]],
+    nutrition_quantities=[["Food", "Category"], ["Quantity"]])
+
+# Define the foreign key relationships
+input_schema.add_foreign_key("nutrition_quantities", "foods", ["Food", "Name"])
+input_schema.add_foreign_key("nutrition_quantities", "categories",
+                            ["Category", "Name"])
+
+# Define the data types
+input_schema.set_data_type("categories", "Min Nutrition", min=0, max=float("inf"),
+                           inclusive_min=True, inclusive_max=False)
+input_schema.set_data_type("categories", "Max Nutrition", min=0, max=float("inf"),
+                           inclusive_min=True, inclusive_max=True)
+input_schema.set_data_type("foods", "Cost", min=0, max=float("inf"),
+                           inclusive_min=True, inclusive_max=False)
+input_schema.set_data_type("nutrition_quantities", "Quantity", min=0, max=float("inf"),
+                           inclusive_min=True, inclusive_max=False)
+
+# We also want to insure that Max Nutrition doesn't fall below Min Nutrition
+input_schema.add_data_row_predicate(
+    "categories", predicate_name="Min Max Check",
+    predicate=lambda row : row["Max Nutrition"] >= row["Min Nutrition"])
+
+# The default-default of zero makes sense everywhere except for Max Nutrition
+input_schema.set_default_value("categories", "Max Nutrition", float("inf"))
+# ---------------------------------------------------------------------------------
+
+
+# ------------------------ define the output schema -------------------------------
+# There are three solution tables, with 3 primary key fields and 3 data fields.
+solution_schema = TicDatFactory(
+    parameters=[["Parameter"], ["Value"]],
+    buy_food=[["Food"], ["Quantity"]],
+    consume_nutrition=[["Category"], ["Quantity"]])
+# ---------------------------------------------------------------------------------
+
+
+# ------------------------ create a solve function --------------------------------
+def solve(dat):
+    """
+    core solving routine
+    :param dat: a good ticdat for the input_schema
+    :return: a good ticdat for the solution_schema, or None
+    """
+    assert input_schema.good_tic_dat_object(dat)
+    assert not input_schema.find_foreign_key_failures(dat)
+    assert not input_schema.find_data_type_failures(dat)
+    assert not input_schema.find_data_row_failures(dat)
+    oops("This will be the error right here!!!!!")
+    # Since nothing else will execute no need for any other lines of code.
+
+# ---------------------------------------------------------------------------------
+
+# ------------------------ provide stand-alone functionality ----------------------
+# when run from the command line, will read/write json/xls/csv/db/sql/mdb files
+if __name__ == "__main__":
+    standard_main(input_schema, solution_schema, solve)
+# ---------------------------------------------------------------------------------
+
