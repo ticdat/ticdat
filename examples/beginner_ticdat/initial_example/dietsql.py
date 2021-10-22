10f6788cc48c89e7313a60770e5cdb82ca7bf2c0
Pete Cacioppi <Peter.Cacioppi@gmail.com>
2021-10-22 10:00:45 -0700

    typo in simple example file

--- examples/beginner_ticdat/initial_example/dietsql.py
+++ examples/beginner_ticdat/initial_example/dietsql.py
@@ -1,6 +1,5 @@
 # Read/write from SQLite database file. Analgous to this file. https://bit.ly/3aB6Zrp
 from diet import solve, input_schema, solution_schema
-from diet import solve, input_schema, solution_schema
 
 dat = input_schema.sql.create_tic_dat("diet.db") # Just look for the diet.db file in the current directory
 
