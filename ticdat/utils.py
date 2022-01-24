4e99aec3217dc9ddfec662c16f95d81c631262eb
Peter Cacioppi <Peter.Cacioppi@gmail.com>
2022-01-23 16:46:04 -0800

    missed a deep_copy case

--- ticdat/utils.py
+++ ticdat/utils.py
@@ -887,7 +887,7 @@ def deep_copy(x):
     :param x: the object to deep copy. should be nested dicts, tuples, lists or TicDatFactory/PanDatFactory
     :return: a deep (and unfrozen, other than tuples) copy of x
     '''
-    if isinstance(x, (tuple, str)) or numericish(x):
+    if isinstance(x, (tuple, str)) or numericish(x) or x is None:
         return x # tuples are frozen anyway
     if isinstance(x, frozenset):
         return frozenset({deep_copy(y) for y in x})
