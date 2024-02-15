cdb6acdc27aadf99e8c7200cd07aeb69f0e42c8f
Pete Cacioppi <Peter.Cacioppi@gmail.com>
2024-02-14 16:21:42 -0800

    add `obfusimplify` to PanDatFactory

--- ticdat/testing/testpandat_utils.py
+++ ticdat/testing/testpandat_utils.py
@@ -930,6 +930,34 @@ class TestUtils(unittest.TestCase):
                          foods = [[f"food_{_}", 10] for _ in range(1, 0)]) # empty list results
         self.assertTrue(dat._len_dict() == {"nutritionQuantities": 10})
 
+
+    def test_obfusimplify(self):
+        # only need a simple test since the heavy work is being done by TicDatFactory
+        pdf = PanDatFactory(products=[["Name"], []], recipes=[["Name"], []],
+                            bill_of_materials=[["Recipe", "Component", "Assembly"], ["Quantity"]],
+                            byproduct=[['Recipe', 'Assembly', 'Byproduct'], ['Quantity']])
+        pdf.add_foreign_key('bill_of_materials', 'products', ('Component', 'Name'))
+        pdf.add_foreign_key('bill_of_materials', 'products', ('Assembly', 'Name'))
+        pdf.add_foreign_key('bill_of_materials', 'recipes', ('Recipe', 'Name'))
+        pdf.add_foreign_key('byproduct', 'bill_of_materials', (('Recipe', 'Recipe'), ('Assembly', 'Assembly')))
+        pdf.add_foreign_key('byproduct', 'products', ('Byproduct', 'Name'))
+        dat = pdf.PanDat(products=[["A"], ["B"], ["C"]], recipes=[["hey"], ["now"]],
+                         bill_of_materials=[["hey", "A", "B", 1], ["now", "A", "B", 112],
+                                            ["now", "C", "B", 1.12]],
+                         byproduct=[["hey", "B", "C", 0.12], ["now", "B", "A", 7]])
+        self.assertTrue(not pdf.find_foreign_key_failures(dat))
+        copy, renamings = pdf.obfusimplify(dat)
+        self.assertFalse(pdf.find_foreign_key_failures(copy))
+        self.assertTrue(dat._len_dict() == copy._len_dict())
+        self.assertFalse(set(dat.products["Name"]).intersection(copy.products["Name"]) or
+                         set(dat.recipes["Name"]).intersection(copy.recipes["Name"]))
+        tic_dat = pdf.copy_to_tic_dat(dat)
+        copy_dat = pdf.copy_to_tic_dat(copy)
+        for (r, a, by), row in copy_dat.byproduct.items():
+            self.assertTrue(tic_dat.byproduct[renamings[r][1], renamings[a][1], renamings[by][1]]["Quantity"] ==
+                            row["Quantity"] > 0)
+
+
 # Run the tests.
 if __name__ == "__main__":
     if not DataFrame :
