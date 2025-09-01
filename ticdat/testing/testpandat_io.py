6dfe03d37492bd2e9bd8511614376aefd31d47a0
Pete Cacioppi <Peter.Cacioppi@gmail.com>
2025-09-01 15:27:23 -0700

    stuff failing here
    very aggravating

--- ticdat/testing/testpandat_io.py
+++ ticdat/testing/testpandat_io.py
@@ -92,6 +92,16 @@ class TestIO(unittest.TestCase):
             self.assertTrue("TicDatError" in e.__class__.__name__)
             return str(e)
 
+    def test_quickie(self):
+        tdf = TicDatFactory(this=[["Field One"], []])
+        tdf2 = TicDatFactory(this=[["Field One"], ["Field Two"]])
+        dat = tdf.TicDat(this=[["a"], ["b"]])
+        s = self.firesException(lambda :  tdf2.json.create_tic_dat(tdf.json.write_file(dat, "")))
+        self.assertTrue("this cannot be treated as a ticDat table : Inconsistent data row lengths." in s)
+        pdf2 = tdf2.clone(clone_factory=PanDatFactory)
+        s = self.firesException(lambda : pdf2.json.create_pan_dat(tdf.json.write_file(dat, "")))
+        self.assertTrue("this cannot be treated as a PanDat table : insufficient number of columns." in s)
+
     def testXlsSimple(self):
         if not self.can_run:
             return
